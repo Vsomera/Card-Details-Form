51c5dada28d34ad4796092082e932bb0fba32ea1
Vilmor Somera <91754316+Vsomera@users.noreply.github.com>
2023-01-17 20:08:40 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -20,44 +20,45 @@ const getCardHolderName = () => {
 
 const getCardNumbers = () => {
     //Function gets the user card numbers and validates it using the validate card function//
-    let userCardNum  = $("#card-name").val();
-    if (validateCard(userCardNum) == true) {
+    let userCardNum  = $("#card-num").val();
+    if (validateCard(userCardNum) == true || userCardNum == "") {
+        $("#card-error").css("display", "none")
         cardNumber = userCardNum
     } else {
         $("#card-error").css("display", "inline")
     }
 }
 
-// const validateCard = (cardNumber) => {
-//     /**
-//         Function uses luhns algorithm to verify credit card number
-//     **/
-//     // Reverse the card number
-//     var reversed = cardNumber.split("").reverse().join("");
+const validateCard = (cardNumber) => {
+    /**
+        Function uses luhns algorithm to verify credit card number
+    **/
+    // Reverse the card number
+    var reversed = cardNumber.split("").reverse().join("");
     
-//     // Multiply every other digit by 2, starting with the first digit
-//     var multiplied = [];
-//     for (var i = 0; i < reversed.length; i++) {
-//       var digit = parseInt(reversed[i]);
-//       if (i % 2 === 0) {
-//         digit *= 2;
-//       }
-//       multiplied.push(digit);
-//     }
+    // Multiply every other digit by 2, starting with the first digit
+    var multiplied = [];
+    for (var i = 0; i < reversed.length; i++) {
+      var digit = parseInt(reversed[i]);
+      if (i % 2 === 0) {
+        digit *= 2;
+      }
+      multiplied.push(digit);
+    }
   
-//     // Subtract 9 from any digits that are greater than 9
-//     var subtracted = multiplied.map(function(digit) {
-//       if (digit > 9) {
-//         digit -= 9;
-//       }
-//       return digit;
-//     });
+    // Subtract 9 from any digits that are greater than 9
+    var subtracted = multiplied.map(function(digit) {
+      if (digit > 9) {
+        digit -= 9;
+      }
+      return digit;
+    });
   
-//     // Sum all the digits
-//     var sum = subtracted.reduce(function(acc, digit) {
-//       return acc + digit;
-//     }, 0);
+    // Sum all the digits
+    var sum = subtracted.reduce(function(acc, digit) {
+      return acc + digit;
+    }, 0);
   
-//     // If the sum is divisible by 10, the card number is valid
-//     return sum % 10 === 0;
-//   }
+    // If the sum is divisible by 10, the card number is valid
+    return sum % 10 === 0;
+  }
